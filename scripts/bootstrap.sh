617e8152aa07b75891c0e4dc30e36e36c2241be4
Fabio Rocha <frock81@yahoo.com.br>
2019-11-21 12:52:09 -0300

    Apt-get update only in 1 day

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,7 +2,9 @@
 
 echo '* libraries/restart-without-asking boolean true' \
   | debconf-set-selections
-apt-get update --fix-missing && apt-get upgrade
+if [ -z "$(find /var/cache/apt/pkgcache.bin -mmin -1440)" ]; then
+  apt-get update --fix-missing && apt-get -yq upgrade
+fi
 test -e /usr/bin/python || apt-get install -yq python-minimal
 test -e /usr/bin/pip || apt-get install -yq python-pip
 test -e /usr/bin/ansible || pip install ansible==2.7.14
