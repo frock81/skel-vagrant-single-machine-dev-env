773c4d7ceded21b520b0a4b5e16749f6a1ec9a70
Fabio Rocha <frock81@yahoo.com.br>
2020-01-31 18:56:06 -0300

    Minor refactor bootstrap

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
+
+# To fix dpkg error in provisioning
 export DEBIAN_FRONTEND=noninteractive
+
 echo '* libraries/restart-without-asking boolean true' \
   | debconf-set-selections
+
 if [ -z "$(find /var/cache/apt/pkgcache.bin -mmin -1440)" ]; then
   apt-get update --fix-missing && apt-get -yq upgrade
 fi
+
 test -e /usr/bin/python || apt-get install -yq python-minimal
 if ! test -e /usr/bin/pip; then
-  apt-get update --fix-missing
+  apt-get update
   apt-get install -yq python-pip
 fi
 test -e /usr/bin/ansible-galaxy || pip install ansible
 ansible-galaxy install \
     -r /vagrant/provision/requirements.yml \
-    -p /vagrant/provision/roles
+    -p /vagrant/provision/roles \
+    --ignore-errors
