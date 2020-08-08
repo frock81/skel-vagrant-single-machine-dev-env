c8729b91644bc3863f00431f95ce971a481039c4
Fabio Rocha <frock81@yahoo.com.br>
2020-08-08 13:01:40 -0300

    Fix bootstrap errors
    Fix bootstrap script problems while checking update, python3-pip
    and Ansible.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -13,41 +13,21 @@ export DEBIAN_FRONTEND=noninteractive
 echo '* libraries/restart-without-asking boolean true' \
   | sudo debconf-set-selections
 
-# FIXME: this seems to be ineffective since the conditional evaluates
-# to false for the first time.
-# Use recent packages.
-# if [ -z "$(find /var/cache/apt/pkgcache.bin -mmin -1440)" ]; then
-#   sudo apt update --fix-missing && apt -yq upgrade
-# fi
+# Some packages may be missing because the new box doesn't have
+# repositories list, although it is configured in the sources.
+if ! $(ls -lh /var/lib/apt/lists | grep -E "universe|multiverse" &> /dev/null); then
+  sudo apt-get update --fix-missing && apt-get -yq upgrade
+fi
 
-# FIXME: this works because the box comes with python3 installed. A
-# better approach would be to inspect lists directory. The first time
-# a Ubuntu box run there is no list for universe or multiverse. As a
-# note, I inspected these files but they all have updated modification
-# time:
-#
-#   - /var/lib/apt/periodic/update-success-stamp
-#   - /var/cache/apt/pkgcache.bin
-#
-# reference command: `ll /var/lib/apt/lists/ | grep -E 'univers|multiv'`
-# Also, it is important to review the necessity of python installation.
-# The apt update is necessary since some packages will not be available
-# without that.
-#
-# In the first vagrant up, python wont be installed. We use this characteristic
-# # to update apt sources file.
-if ! test -e /usr/bin/python; then
-  sudo sed -i 's#http://archive.ubuntu.com#http://br.archive.ubuntu.com#g' \
-    /etc/apt/sources.list
-  echo "Updating packages list"
-  sudo apt update
-  sudo echo "Installing python."
-  apt install -yq python-minimal python-pip
+if ! $(dpkg -s python3-pip &> /dev/null); then
+  sudo apt-get install -y python3-pip
 fi
 
 # Install Ansible via Python PIP.
-echo "Installing Ansible via PIP"
-test -e /usr/bin/ansible-galaxy || sudo pip install ansible
+if ! test -e /usr/local/bin/ansible-galaxy; then
+  echo "Installing Ansible via Pip"
+  sudo pip3 install ansible
+fi
 
 # Install required roles.
 if [ -r "$ANSIBLE_PATH/requirements.yml" ]; then
