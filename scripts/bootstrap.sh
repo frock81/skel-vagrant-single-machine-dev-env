4d8dc0a1efb15b93abaa86c67c5542bd86d6455d
Fabio Rocha <frock81@yahoo.com.br>
2020-08-08 11:33:24 -0300

    Comment ineffective code in bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -14,24 +14,24 @@ echo '* libraries/restart-without-asking boolean true' \
   | sudo debconf-set-selections
 
 # Use recent packages.
-if [ -z "$(find /var/cache/apt/pkgcache.bin -mmin -1440)" ]; then
-  sudo apt update --fix-missing && apt -yq upgrade
-fi
+# if [ -z "$(find /var/cache/apt/pkgcache.bin -mmin -1440)" ]; then
+#   sudo apt update --fix-missing && apt -yq upgrade
+# fi
 
-# In the first vagrant up, python wont be installed. We use this characteristic
-# to update apt sources file.
-if ! test -e /usr/bin/python; then
-  sudo sed -i 's#http://archive.ubuntu.com#http://br.archive.ubuntu.com#g' \
-    /etc/apt/sources.list
-  echo "Updating packages list"
-  sudo apt update
-  sudo echo "Installing python."
-  apt install -yq python-minimal python-pip
-fi
+# # In the first vagrant up, python wont be installed. We use this characteristic
+# # to update apt sources file.
+# if ! test -e /usr/bin/python; then
+#   sudo sed -i 's#http://archive.ubuntu.com#http://br.archive.ubuntu.com#g' \
+#     /etc/apt/sources.list
+#   echo "Updating packages list"
+#   sudo apt update
+#   sudo echo "Installing python."
+#   apt install -yq python-minimal python-pip
+# fi
 
 # Install Ansible via Python PIP.
-echo "Installing Ansible via PIP"
-test -e /usr/bin/ansible-galaxy || sudo pip install ansible
+# echo "Installing Ansible via PIP"
+# test -e /usr/bin/ansible-galaxy || sudo pip install ansible
 
 # Install required roles.
 if [ -r "$ANSIBLE_PATH/requirements.yml" ]; then
